e2cfa33d19e3f3430653a4122289073a4a610b1e
Kirthana <137603681+kirthana1181@users.noreply.github.com>
2025-12-13 12:57:46 +0530

    Create gemfile.rb

---
+++ gemfile.rb
@@ -0,0 +1,29 @@
+source "https://rubygems.org"
+
+# Jekyll version
+gem "jekyll", "~> 4.3.2"
+
+# Default theme
+gem "minima", "~> 2.5"
+
+# Jekyll plugins
+group :jekyll_plugins do
+  gem "jekyll-feed", "~> 0.12"
+  gem "jekyll-seo-tag"
+  gem "jekyll-sitemap"
+end
+
+# Windows and JRuby specific
+platforms :mingw, :x64_mingw, :mswin, :jruby do
+  gem "tzinfo", ">= 1", "< 3"
+  gem "tzinfo-data"
+end
+
+# Performance-booster for watching directories on Windows
+gem "wdm", "~> 0.1.1", :platforms => [:mingw, :x64_mingw, :mswin]
+
+# Lock `http_parser.rb` gem to `v0.6.x` on JRuby builds
+gem "http_parser.rb", "~> 0.6.0", :platforms => [:jruby]
+
+# GitHub Pages gem (optional - uncomment if deploying to GitHub Pages)
+# gem "github-pages", group: :jekyll_plugins
