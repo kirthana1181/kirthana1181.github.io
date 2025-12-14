be417e9bbe0901fcb61edb2cb306a4db69f6af5a
Kirthana <137603681+kirthana1181@users.noreply.github.com>
2025-12-14 13:04:18 +0530

    Update gemfile.rb

--- gemfile.rb
+++ gemfile.rb
@@ -1,16 +1,14 @@
 source "https://rubygems.org"
 
-# Jekyll version
 gem "jekyll", "~> 4.3.2"
+gem "jekyll-remote-theme"
+gem "jekyll-include-cache"
 
-# Default theme
-theme "minima", "~> 2.5"
-
-# Jekyll plugins
 group :jekyll_plugins do
-  gem "jekyll-feed", "~> 0.12"
+  gem "jekyll-feed"
   gem "jekyll-seo-tag"
   gem "jekyll-sitemap"
+  gem "jekyll-paginate"
 end
 
 # Windows and JRuby specific
