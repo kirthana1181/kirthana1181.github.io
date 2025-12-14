15681e9d4f58c2b6a1343b369e177df7c8e34f29
Kirthana <137603681+kirthana1181@users.noreply.github.com>
2025-12-14 11:08:17 +0530

    Update gemfile.rb

--- gemfile.rb
+++ gemfile.rb
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 gem "jekyll", "~> 4.3.2"
 
 # Default theme
-gem "minima", "~> 2.5"
+jekyll-remote-theme "minima", "~> 2.5"
 
 # Jekyll plugins
 group :jekyll_plugins do
